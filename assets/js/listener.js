206fce4b5a9612660ee512ec4f4cee15ef5d6bef
joutria <joutria@gmail.com>
2021-06-03 19:12:05 -0500

    removed most of the js code and added sections

---
+++ assets/js/listener.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    const menu = document.querySelector(".menu_mobile");
+
+    menu.addEventListener = function(){}
+
+    menu.onclick= function(){
+        console.log(menu);
+        menu.styles.height="10em";
+    }
+}
